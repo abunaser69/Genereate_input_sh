f924180b564dfdc9f3bff401e75920376efa07f7
Abu Naser <31278357+abunaser69@users.noreply.github.com>
2022-09-13 16:21:33 +0100

    Update loop_for_atom_type.sh

--- loop_for_atom_type.sh
+++ loop_for_atom_type.sh
@@ -3,3 +3,10 @@
  do
          echo -e "1\n2\n3\n4\n5\n6\n6\n7\n7\n7\n7\n6\n6"
  done
+
+
+#!/bin/bash
+ for i in `seq 1 441`;
+ do
+         echo -e "$((i+1))   $((i+2))\n$i   $((i+3))   $((i+4))   $((i+5))\n$((i+1))\n$((i+1))\n$((i+1))"
+ done
